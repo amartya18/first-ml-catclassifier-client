d67789b8344ca04d5e99faa20857936e5f3e52ab
amartya <amartya.sk@gmail.com>
2020-09-13 20:45:09 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+import { useEffect } from 'react';
+import Spinner from 'react-bootstrap/Spinner'
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
+  const [imgpath, setImgpath] = useState('https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png');
+  const [label, setLabel] = useState(null);
+  const [probability, setProbability] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const imageHandler = (e) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setImgpath(reader.result);
+      }
+    }
+    sendImg(e.target.files[0])
+    reader.readAsDataURL(e.target.files[0])
+  }
+
+  const sendImg = async (imageFile) => {
+    setLoading(true);
+    const formData = new FormData();
+    formData.append('image', imageFile)
+    try {
+      const data = await axios
+        .post('https://rest-first-cat-classifier.herokuapp.com', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        })
+        .then(res => {
+          console.log(res.data);
+          setLabel(res.data.label);
+          setProbability(res.data.probability);
+          setLoading(false);
+        })
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row className="justify-content-center text-center">
+          <Col>
+              <h1 className="heading">Simple Deep Learning Cat Classifier</h1>
+              <div className="img-holder">
+                <img src={imgpath} alt="" />
+              </div>
+              <h3>
+                {loading ? <Spinner animation="border" /> : <p>{label} {probability}</p>}
+              </h3>
+              <input type="file" accept="image/*" name="image-upload" onChange={imageHandler} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
