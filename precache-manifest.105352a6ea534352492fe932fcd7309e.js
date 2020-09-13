ec53d3e61c6746a168ebf23621b2a3aef39abf6f
amartya <amartya.sk@gmail.com>
2020-09-13 20:56:47 +0700

    Updates

---
+++ precache-manifest.105352a6ea534352492fe932fcd7309e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b10c806def446f3515a9194c8aa3a1bf",
+    "url": "/first-ml-catclassifier-client/index.html"
+  },
+  {
+    "revision": "a785058a27cc8164e1bc",
+    "url": "/first-ml-catclassifier-client/static/css/main.b2eba036.chunk.css"
+  },
+  {
+    "revision": "5c74437ee6942ab9381f",
+    "url": "/first-ml-catclassifier-client/static/js/2.e7dd74f8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/first-ml-catclassifier-client/static/js/2.e7dd74f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a785058a27cc8164e1bc",
+    "url": "/first-ml-catclassifier-client/static/js/main.a80bdf82.chunk.js"
+  },
+  {
+    "revision": "d314c9079668c3d03eb0",
+    "url": "/first-ml-catclassifier-client/static/js/runtime-main.476f9f76.js"
+  }
+]);
